fc9c5292f09342a9a4655ac33032cdfad6265d8f
Sakenclaine <87309945+Sakenclaine@users.noreply.github.com>
2024-08-28 19:34:50 +0200

    check for tray icon capability

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QMessageBox>
+#include <QSystemTrayIcon>
 
 #include "mainwindow.h"
 #include "winController.h"
@@ -12,6 +14,22 @@ int main(int argc, char* argv[])
 
     // Setup main application 
     QApplication a(argc, argv);
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        auto choice = QMessageBox::critical(
+            nullptr,
+            QObject::tr("Systray"),
+            QObject::tr("I couldn't detect any system tray on this system."),
+            QMessageBox::Close | QMessageBox::Ignore
+        );
+
+        if (choice == QMessageBox::Close)
+            return 1;
+        // Otherwise "lurk": if a system tray is started later, the icon will appear.
+    }
+
+    // Prevent program from closing, when closing main window without tray icon
+    QApplication::setQuitOnLastWindowClosed(false);
 
     // Create main window and show it
     MainWindow w;
